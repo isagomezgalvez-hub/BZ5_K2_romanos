848c0cc1044894432c4243565714ac564c7f57d6
Isabel Gomez <isa@MacBook-Pro-de-Isabel.local>
2020-05-07 23:07:32 +0200

    Clase Romanos

---
+++ cromanos_test.py
@@ -0,0 +1,31 @@
+import unittest
+
+import cromanos
+
+
+class RomanNumberTest(unittest.TestCase):
+
+    def test_crea_romanos(self):
+        nr = cromanos.RomanNumber(25)
+        self.assertEqual(nr.value, 25)
+        self.assertEqual(nr.rvalue, 'XXV')
+
+        snr = cromanos.RomanNumber('XXIV')
+        self.assertEqual(snr.value, 24)
+        self.assertEqual(snr.rvalue, 'XXIV')
+
+        tnr = cromanos.RomanNumber('XXXX')
+        self.assertEqual(tnr.value, 'Error en formato')
+        self.assertEqual(tnr.rvalue, 'Error en formato')
+
+        cnr = cromanos.RomanNumber(0)
+        self.assertEqual(cnr.value, 'Overflow')
+        self.assertEqual(cnr.rvalue, 'Overflow')
+
+        qnr = cromanos.RomanNumber(4000)
+        self.assertEqual(qnr.value, 'Overflow')
+        self.assertEqual(qnr.rvalue, 'Overflow')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ trabajando_con_funciones.py
@@ -0,0 +1,10 @@
+def doble(n):
+    return n*2
+
+
+class Persona():
+    def __init__(self, nombre, edad):
+        self.name = nombre
+        self.age = edad
+
+Rober = Persona('Roberto', 30)
